e740b019cb4607cbfeb67aba82f625d6322bf43b
AGX <agxnh2020@gmail.com>
2026-04-16 22:42:17 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
@@ -29,16 +32,54 @@ func main() {
 		}
 		logger.Println("Client connected:", conn.RemoteAddr())
 		// reading and parsing RESP commands from the client and responding with a simple PONG message
-		conn.Write([]byte(parseRESP(bufio.NewReader(conn))))
-		conn.Close()
+		HandleConnection(conn)
 	}
 }
 
-func parseRESP(reader *bufio.Reader) string {
-	_, err := reader.ReadString('\n')
+func HandleConnection(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	args, err := parseRESP(reader)
+
 	if err != nil {
-		return ""
+		conn.Write([]byte("-ERR " + err.Error() + "\r\n"))
+		return
 	}
 
-	return "+PONG\r\n"
+	if len(args) == 0 {
+		conn.Write([]byte("-ERR No command provided\r\n"))
+		return
+	}
+
+	conn.Write([]byte("+PONG\r\n"))
+}
+
+func parseRESP(reader *bufio.Reader) ([]string, error) {
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+
+	line = strings.TrimSpace(line)
+
+	if line[0] != '*' {
+		return nil, fmt.Errorf("expected array, got %s", line)
+	}
+
+	count, _ := strconv.Atoi(line[1:])
+	args := make([]string, 0, count)
+
+	for i := 0; i < count; i++ {
+		// Read the $N line
+		reader.ReadString('\n')
+
+		// Read the actual value
+		value, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+		args = append(args, strings.TrimSpace(value))
+	}
+
+	return args, nil
 }
