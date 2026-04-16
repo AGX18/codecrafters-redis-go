7a7b9a48ff5be4fb28bede035ddbea0fee9bea74
AGX <agxnh2020@gmail.com>
2026-04-16 22:19:54 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"log"
 	"net"
 	"os"
 )
@@ -11,19 +12,33 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	logger := log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
 
-	// Uncomment the code below to pass the first stage
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	logger.Println("Starting the Program")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		logger.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			logger.Println("Error accepting connection:", err)
+			continue
+		}
+		logger.Println("Client connected:", conn.RemoteAddr())
+		// reading and parsing RESP commands from the client and responding with a simple PONG message
+		conn.Write([]byte(parseRESP(bufio.NewReader(conn))))
+		conn.Close()
 	}
+}
+
+func parseRESP(reader *bufio.Reader) string {
+	_, err := reader.ReadString('\n')
+	if err != nil {
+		return ""
+	}
+
+	return "+PONG\r\n"
 }
